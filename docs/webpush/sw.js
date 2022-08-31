549631750a7cce24927a9b7e4378642486055af4
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2022-08-31 15:27:30 +0800

    update

--- docs/webpush/sw.js
+++ docs/webpush/sw.js
@@ -1,15 +1,16 @@
 self.addEventListener('push', function (event) {
-  const { icon, title, message } = JSON.parse(event.data.text())
+  const { icon, title, message, data } = JSON.parse(event.data.text())
   event.waitUntil(
     registration.showNotification(title, {
       body: message,
+      data,
       icon: icon || 'https://www.home-assistant.io/images/favicon-192x192.png'
     })
   );
 });
 
 self.addEventListener('notificationclick', function (event) {
-  const { url } = JSON.parse(event.data.text())
+  const { url } = event.notification.data
   event.notification.close();
   event.waitUntil(clients.openWindow(url));
 });
