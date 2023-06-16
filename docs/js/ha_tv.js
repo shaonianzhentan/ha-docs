525047ef9bb6fae70980e08047d310da40ede552
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2023-06-17 00:01:42 +0800

    update

--- docs/js/ha_tv.js
+++ docs/js/ha_tv.js
@@ -1,2 +1,22 @@
-console.log('此脚本注入到Home Assistant TV版本')
-
+const HAStyle = {
+  'tv.cctv.com': `
+      #player{
+        width: 100% !important;
+        height: 100% !important;
+        position: fixed !important;
+        left: 0 !important;
+        top: 0 !important;
+      }
+      .zhibo_201014,
+      .gwA18043_ind01{
+        display: none !important;
+      }
+  `,
+  'live.douyin.com': `
+  `
+}
+if (location.hostname in HAStyle) {
+  const style = document.createElement('style')
+  style.textContent = HAStyle[location.hostname]
+  document.body.appendChild(style)
+}
